8d438288102a647de9c78105cb4312b11e1fff95
ahmedee0 <akarray276@example.com>
2023-06-20 18:36:53 -0700

    last

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,9 +1,8 @@
 #include "main.h"
 
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * jack_bauer - Prints every minute of the day of
+ * Jack Bauer, starting from 00:00 to 23:59.
  */
 
 void jack_bauer(void)
