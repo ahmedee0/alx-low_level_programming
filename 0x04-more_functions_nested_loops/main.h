9a91d218dd82e66892880e43604edb42f5afb2b8
ahmedee0 <akarray276@example.com>
2023-06-23 10:47:46 -0700

    .

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -2,6 +2,7 @@
 #define MAIN_H
 
 
+int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
@@ -14,5 +15,8 @@ void print_square(int size);
 void print_triangle(int size);
 void print_number(int n);
 
-#endif
 
+#endif 
+/* 
+Bwave ICT 
+*/
