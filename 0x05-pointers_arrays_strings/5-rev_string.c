17fcbae442b9f093882c08755a76d46b7e4ca695
ahmedee0 <akarray276@example.com>
2023-06-26 03:23:03 -0700

    5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,18 @@
+void rev_string(char *s)
+{
+	int i = 0, a, b;
+	char rev;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	b = i - 1;
+
+	for (a = 0 ; b >= 0 && a < b; b--, a++)
+	{
+		d = s[b];
+		s[b] = s[a];
+		s[a] = d;
+	}
+}
